30f19209ccdde0e5fd10776adf1e4699d6224b05
Neil Stevens <neil@stevensmedia.us>
2018-01-30 20:26:05 -0500

    Handle the case of no stdin

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 var ravensgleaning = require('ravens-gleaning');
 var fs = require('fs');
 
-var ansi = fs.readFileSync(process.stdin.fd);
+try {
+	var ansi = fs.readFileSync(process.stdin.fd);
+} catch(err) {
+	if(err.message.match('EAGAIN')) {
+		return;
+	}
+	throw(err);
+}
 var html = ravensgleaning.html(ansi);
 fs.writeSync(process.stdout.fd, html);
